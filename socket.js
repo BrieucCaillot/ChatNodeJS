a1d1fe5c6af37b715a1d1fd82ed20422e48f5288
Alexandre Bétourné <AlexandreB@MacBook-Pro-de-Alexandre-2.local>
2017-03-27 16:27:23 +0200

    style

--- socket.js
+++ socket.js
@@ -7,19 +7,6 @@ var port = 1337;
 io.on('connection', function(socket){
 	console.log('new socket');
 
-	/*socket.on('message', function(data){
-		console.log('Server : new event \'test\' received with value');
-		console.log(data);
-
-		socket.emit('response', {
-			success : true,
-			message : data.msg,
-			timestamp_received : data.timestamp,
-			timestamp_sent : new Date().getTime(),
-			response : 'hello'
-		});
-	});*/
-
 	socket.on('message', function(message){
 		console.log('received', message);
 		io.emit('newmessage', message);
@@ -31,16 +18,8 @@ app.use('/static/js', express.static(__dirname + '/assets/js'));
 app.use('/static/fonts', express.static(__dirname + '/assets/fonts'));
 app.use('/static/images', express.static(__dirname + '/assets/images'));
 
-app.get('/index', function(req, res){
-	res.sendFile(__dirname + '/html/index.html');
-});
-
-app.get('/contact', function(req, res){
-	res.sendFile(__dirname + '/html/contact.html');
-});
-
 app.get('/', function(req, res){
-	res.sendFile(__dirname + '/html/socket.html');
+	res.sendFile(__dirname + '/index.html');
 });
 
 server.listen(1337);
