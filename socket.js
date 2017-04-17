ac2c76aacc252e751221b490ccadb98b804a2e2c
BrieucKyo <brieucmailpro@gmail.com>
2017-04-17 17:08:47 +0200

    Added ent and modif chat

--- socket.js
+++ socket.js
@@ -1,17 +1,9 @@
 var express = require('express'); 
 var app = express();
 var server = require('http').createServer(app);
-var io = require('socket.io')(server);
+var io = require('socket.io').listen(server);
+var ent = require('ent');
 var port = 1337;
-
-io.on('connection', function(socket){
-	console.log('new socket');
-
-	socket.on('message', function(message){
-		console.log('received', message);
-		io.emit('newmessage', message);
-	});
-});
 
 app.use('/static/css', express.static(__dirname + '/assets/css'));
 app.use('/static/js', express.static(__dirname + '/assets/js'));
@@ -22,5 +14,28 @@ app.get('/', function(req, res){
 	res.sendFile(__dirname + '/index.html');
 });
 
-server.listen(1337);
+io.sockets.on('connection', function (socket, pseudo) {
+    // Dès qu'on nous donne un pseudo, on le stocke en variable de session et on informe les autres personnes
+    socket.on('nouveau_client', function(pseudo) {
+        pseudo = ent.encode(pseudo);
+        socket.pseudo = pseudo;
+        socket.broadcast.emit('nouveau_client', pseudo);
+    });
+
+    // Dès qu'on reçoit un message, on récupère le pseudo de son auteur et on le transmet aux autres personnes
+    socket.on('message', function (message) {
+        message = ent.encode(message);
+        socket.broadcast.emit('message', {pseudo: socket.pseudo, message: message});
+    }); 
+});
+
+server.listen(port);
 	console.log('Server listening on port 1337');
+
+
+
+
+
+
+
+
