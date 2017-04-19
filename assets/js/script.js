0946334a8d6c02675e16b800fa59982ae0bd894a
BrieucKyo <brieucmailpro@gmail.com>
2017-04-19 17:03:47 +0200

    Modif from develop

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,87 @@
 jQuery(document).ready(function($) {
 
+	$("#skyblue").click(function(){
+		$("body").css("background-color", "skyblue");
+		$("body").addClass('transition');
+		$("#zone_chat strong").css({
+			"color": '#fff',
+			"background-color": 'skyblue'
+		});
+		
+		$("video").css("display", "block");
+
+		var vid = document.getElementsByTagName('video')[0]; 
+		vid.play(); 
+
+		var videoend = function(){
+        	$("video").fadeOut();
+        	$("video").attr("muted", "true");
+    	};
+
+    	setTimeout(videoend, 4500);
+		/*$("#gif img").css("src=", "../static/images/wow.gif");*/
+	});
+
+	$("#blue").click(function(){
+		$("body").css("background-color", "blue");
+		$("body").addClass('transition');
+		$("#zone_chat strong").css({
+			"color": '#fff',
+			"background-color": 'blue'
+		});
+
+		$("video").css("display", "block");
+
+		var vid = document.getElementsByTagName('video')[0]; 
+		vid.play(); 
+
+		var videoend = function(){
+        	$("video").fadeOut();
+        	$("video").attr("muted", "true");
+    	};
+
+    	setTimeout(videoend, 4500);
+	});
+
+	$("#emo").click(function(){
+		$("body").css("background-color", "#000");
+		$("body").addClass('transition');
+		$("#zone_chat strong").css({
+			"color": '#fff',
+			"background-color": '#000'
+		});
+
+		$("video").css("display", "block");
+
+		var vid = document.getElementsByTagName('video')[0]; 
+		vid.play(); 
+
+		var videoend = function(){
+        	$("video").fadeOut();
+        	$("video").attr("muted", "true");
+    	};
+
+    	setTimeout(videoend, 4500);
+	});
+
+	$("#red").click(function(){
+		$("body").css("background-color", "red");
+		$("body").addClass('transition');
+		$("#zone_chat strong").css({
+			"color": '#fff',
+			"background-color": 'red'
+		});
+
+		$("video").css("display", "block");
+
+		var vid = document.getElementsByTagName('video')[0]; 
+		vid.play(); 
+
+		var videoend = function(){
+        	$("video").fadeOut();
+        	$("video").attr("muted", "true");
+    	};
+
+    	setTimeout(videoend, 4500);
+	});
 });
--- socket.js
+++ socket.js
@@ -4,6 +4,7 @@ var server = require('http').createServer(app);
 var io = require('socket.io').listen(server);
 var ent = require('ent');
 var port = 1337;
+var fs = require('fs');
 
 app.use('/static/css', express.static(__dirname + '/assets/css'));
 app.use('/static/js', express.static(__dirname + '/assets/js'));
@@ -15,14 +16,13 @@ app.get('/', function(req, res){
 });
 
 io.sockets.on('connection', function (socket, pseudo) {
-    // Dès qu'on nous donne un pseudo, on le stocke en variable de session et on informe les autres personnes
+    
     socket.on('nouveau_client', function(pseudo) {
         pseudo = ent.encode(pseudo);
         socket.pseudo = pseudo;
         socket.broadcast.emit('nouveau_client', pseudo);
     });
 
-    // Dès qu'on reçoit un message, on récupère le pseudo de son auteur et on le transmet aux autres personnes
     socket.on('message', function (message) {
         message = ent.encode(message);
         socket.broadcast.emit('message', {pseudo: socket.pseudo, message: message});
@@ -30,7 +30,7 @@ io.sockets.on('connection', function (socket, pseudo) {
 });
 
 server.listen(port);
-	console.log('Server listening on port 1337');
+	console.log('Server listening on port' + port);
 
 
 
